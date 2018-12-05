ebac86b8118aaf45995b54696de0e4c21eba60dd
crisalgarol <crisalgarol@gmail.com>
2018-12-05 17:37:26 -0600

    3

--- PruebasExternas/pruebaDaniel.cpp
+++ PruebasExternas/pruebaDaniel.cpp
@@ -4,17 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <vector>
+#include <ctype.h>
+#include <stdio.h>
  
 using namespace std;
 
-int esPalindromo(char *, int);
+bool esPalindromo(char *, int);
 string conversionAPlano(string);
 string conversionAPlano(string texto);
 vector<string> separarPalabras(string texto, int n);
+string serializar(vector<string> vectorus);
 
 int main () {
 	
-    int n = 0;
+    int n = 5;
     int longitud;
 
     vector <string> palabrasObtenidas;
@@ -25,45 +28,77 @@ int main () {
 	ifstream ifs("prueba.txt");
     string content( (std::istreambuf_iterator<char>(ifs)),(std::istreambuf_iterator<char>()) );
 
-    palabrasObtenidas = separarPalabras(content, 3);
-    
-    for(int i=0; i<palabrasObtenidas.size(); i++){
-        cout << palabrasObtenidas.at(i) << endl;
-    }
+    int contadorDeNVueltas = n;
+
+    while(contadorDeNVueltas >= 2){
+
+            cout << "Iteracion para: " << contadorDeNVueltas << endl;
+            cout << "_________________________________________________-" << endl;
+
+            string listaDePalabras = serializar(palabrasObtenidas = separarPalabras(content, contadorDeNVueltas));
+            separarPalabras(content, contadorDeNVueltas);
+            cout << "Despues de lista" << endl;
+            cout << listaDePalabras << endl;
+            cout << listaDePalabras << endl;
+            for(int i=0; i<palabrasObtenidas.size(); i++){
+
+                cout << palabrasObtenidas.at(i) << endl; 
+
+                string limpio = conversionAPlano(palabrasObtenidas.at(i));//lo 
+                longitud = strlen(limpio.c_str());
+
+                char cadenalimpia[sizeof(limpio)];//declaro cadena limpia
+                strcpy(cadenalimpia, limpio.c_str());//copio ahi lo leido	
+
+                if(esPalindromo(cadenalimpia, longitud))
+                    palindromos.push_back(palabrasObtenidas.at(i));
+                
+            }
 
 
+            cout << "Palindromos encontrados " << endl;
+            for(int i=0; i<palindromos.size(); i++){
+                //Buscar
+                cout << palindromos.at(i) << endl;
+            }
 
-/*
-    string limpio = conversionAPlano(content);//lo 
-    longitud = strlen(limpio.c_str());
-    cout<<longitud<<endl;
+            contadorDeNVueltas--;
+            cout << "_________________FIN________________________________\n\n" << endl << endl;
+        }
 
-    char cadenalimpia[sizeof(limpio)];//declaro cadena limpia
-	strcpy(cadenalimpia, limpio.c_str());//copio ahi lo leido	
-
-    int resultado = esPalindromo(cadenalimpia, longitud);
-    
-    if (resultado) {
-        cout<< cadenalimpia << " \nes palindroma"<< endl;
-    } else {
-        cout<< cadenalimpia <<" \nNO es palindroma"<< endl;
-    }
-    */
 }
 
-int esPalindromo(char * cadena, int longitud) {
-    if (longitud <= 1) return 1; 
+bool esPalindromo(char *cadena, int longitud) {
+    //if (longitud <= 1) return true; 
     int inicio = 0, fin = longitud - 1; 
     // Mientras el primer y último carácter sean iguales
+    for(int i=0; cadena[i]; i++){
+        cadena[i] = tolower(cadena[i]);
+    }
+
     while (cadena[inicio] == cadena[fin]){
         // Aquí sólo resta un carácter por comparar, eso indica que SÍ es palíndroma
-        if (inicio >= fin) return 1;
+        if (inicio >= fin) return true;
         // Vamos acortando la cadena
         inicio++;
         fin--;
     }
+
     // Si termina el ciclo y no se rompió, entonces no es palíndroma
-    return 0;
+    return false;
+}
+
+int contadorEspacios(string cadena){
+
+    int counter = 0;
+
+    for(char &c: cadena){
+        if(c == ' ')
+            counter++;
+    }
+
+    return counter;
+
 }
 
 string conversionAPlano(string texto){
@@ -75,46 +110,7 @@ string conversionAPlano(string texto){
 
     for (char& letra: texto){
 
-        //Si hay un acento o la verirguilla de la n
-        if(letra == (char)-61)
-            continue;
-        
-        //Si es una enie Mayuscula
-        if(letra == (char) -79 )
-            textoLimpio += "}";
-        
-        else if(letra == (char)-111)
-            textoLimpio += "{";
-        
-        
-        //Si es una letra acentuada minuscula
-        else if(letra == (char)-95 || letra == (char)-87 || letra == (char)-83 || letra == (char)-77 || letra == (char)-70){            
-            switch((int)letra){
-                case -95: textoLimpio+=")"; break;
-                case -87: textoLimpio+="="; break;
-                case -83: textoLimpio+="+"; break;
-                case -77: textoLimpio+="-"; break;
-                case -70: textoLimpio+="*"; break;
-                default: break;
-            }
-        }
-
-        
-
-        //Si es una letra acentuada mayuscula
-        else if(letra == (char)-127 || letra == (char)-119 || letra == (char)-115 || letra == (char)-109 || letra == (char)-102){
-            switch((int)letra){
-                case -127: textoLimpio+=")"; break;
-                case -119: textoLimpio+="="; break;
-                case -115: textoLimpio+="+"; break;
-                case -109: textoLimpio+="-"; break;
-                case -102: textoLimpio+="*"; break;
-                default: break;
-            }  
-        }
-
-        
-        else if(((int)letra>=65 && (int)letra<= 90) || ((int)letra>=97 && (int)letra<= 122))
+        if(letra != ' ')
             textoLimpio+=letra;
         
     } 
@@ -126,13 +122,13 @@ vector<string> separarPalabras(string texto, int n){
     vector<string> palabras;
     vector <string> palabrasEnConjuntos;
 
+    int valorAnterior = 0;
+
     string palabraLeida;        
     string conjunto;
 
-
     /***Agrega todas las palabras separadas a palabras***/
     for(char letra: texto){
-
 
         if(letra == ' '){
             palabras.push_back(palabraLeida); 
@@ -155,13 +151,11 @@ vector<string> separarPalabras(string texto, int n){
                 case -95: palabraLeida+=")"; break;
                 case -87: palabraLeida+="="; break;
                 case -83: palabraLeida+="+"; break;
-                case -77: palabraLeida+="-"; break;
+                case -77: palabraLeida+="["; break;
                 case -70: palabraLeida+="*"; break;
                 default: break;
             }
         }
-
-        
 
         //Si es una letra acentuada mayuscula
         else if(letra == (char)-127 || letra == (char)-119 || letra == (char)-115 || letra == (char)-109 || letra == (char)-102){
@@ -169,48 +163,85 @@ vector<string> separarPalabras(string texto, int n){
                 case -127: palabraLeida+=")"; break;
                 case -119: palabraLeida+="="; break;
                 case -115: palabraLeida+="+"; break;
-                case -109: palabraLeida+="-"; break;
+                case -109: palabraLeida+="["; break;
                 case -102: palabraLeida+="*"; break;
                 default: break;
             }  
         }
 
+        else if( (int)letra < 0)
+            continue;
+
         
         else if(((int)letra>=65 && (int)letra<= 90) || ((int)letra>=97 && (int)letra<= 122))
-            palabraLeida+=letra;
+            palabraLeida+= letra;
+        
     }
     palabras.push_back(palabraLeida); 
     /**********************/
+
+
+    int tamanoOriginalArregloPalabras = palabras.size();
         
     while(palabras.size() >= n){
-
-        for(int i=0; i<palabras.size(); i++){
-
-            if(n==1 && i==0){
-                palabrasEnConjuntos.push_back(palabras.at(0));
-                continue;
-            }
-
-            else if( i!= 0 && ( ( (i+1)%n)==0) ){
-
-                if(n!=1)
-                    conjunto += " ";
-
-                conjunto +=palabras.at(i);
-                palabrasEnConjuntos.push_back(conjunto);
-                conjunto = "";
-            }else{
-                conjunto += palabras.at(i);
-                if((i+2) % n != 0)
-                    conjunto += " ";
-            }   
-        }
-        palabras.erase(palabras.begin());
         
-    
-        //cout << "***********" << endl;
+            conjunto = "";
+
+            for(int i=0; i<palabras.size(); i++){
+
+                if(n==1 && i==0){
+                    palabrasEnConjuntos.push_back(palabras.at(0));
+                    continue;
+                }
+
+                else if( i!= 0 && ( ( (i+1)%n)==0) ){
+
+                    if(n!=1)
+                        conjunto += " ";
+
+                    conjunto +=palabras.at(i);
+                    
+                    if(contadorEspacios(conjunto) == (n-1)){
+                        palabrasEnConjuntos.push_back(conjunto);
+                    }
+                    
+                    conjunto = "";
+                    //cout << palabrasEnConjuntos.at(palabrasEnConjuntos.size()-1) << endl;
+                }else{
+                    conjunto += palabras.at(i);
+                    if((i+2) % n != 0)
+                        conjunto += " ";
+                }   
+            }
+        
+            if(n==1)
+                break;
+
+        if(valorAnterior != 0 && valorAnterior!=palabras.size()){
+            cout << "Error, disminuyo" << endl;
+            int bbbb;
+            cin >> bbbb;
+        }
+
+        cout << "PS1: " << palabras.size() << endl;
+        palabras.erase(palabras.begin());
+        //cout << "Pase a la segunda etapa" << endl;
+        valorAnterior = palabras.size();
     }
 
     return palabrasEnConjuntos;
+
+}
+
+string serializar(vector<string> vectorus){
+
+    string devuelto = "";
+
+    for(int i=0; i<vectorus.size(); i++){
+        devuelto += vectorus.at(i) + "|";
+    }
+
+    cout<<"Salida de serializar" << endl;
+    return devuelto;
 
 }
